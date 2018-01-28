f3297e348dd5f0d4a575fc6bc92eb183ae83c7b7
ypcai <chinabaozi@yeah.net>
2018-01-28 16:16:47 +0800

    pod 2.0.52

--- FMMapKit_inroad/AppDelegate.mm
+++ FMMapKit_inroad/AppDelegate.mm
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FMMapKit.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[FMKMapSDK shareSDK] setKey:@"3adc0411995375bac9afe81856feadf4"];
+
     return YES;
 }
 
--- FMMapKit_inroad/ViewController.m
+++ FMMapKit_inroad/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "FMMapKit.h"
 
-@interface ViewController ()
+@interface ViewController ()<FMKMapViewDelegate>
+{
+    FMKMapView *_mapView;
+}
 
 @end
 
@@ -16,13 +20,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 离线地图加载
+//    NSString *dataPath = [[NSBundle mainBundle] pathForResource:@"10347" ofType:@"fmap"];
+//    _mapView = [[FMKMapView alloc] initWithFrame:CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-64) path:dataPath delegate:self];
+//    [self.view addSubview:_mapView];
+    
+    // 在线地图加载
+    _mapView = [[FMKMapView alloc] initWithFrame:CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-64) ID:@"f200203" delegate:self autoUpgrade:YES];
+    [self.view addSubview:_mapView];
+    
+    // 离线主题加载
+    // 地图主题如果不设置，则SDK默认加载FMBundle.bundle中名称为2001的主题数据。
+    //    [_mapView setThemeWithLocalPath:[FMBUNDLE pathForResource:@"2001.theme" ofType:nil]];
+    //    NSString *themePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"3007/3007.theme"];
+    //    [_mapView setThemeWithLocalPath:themePath];
+    
+    // 在线主题加载
+    [_mapView setThemeWithID:@"2001"];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - FMKMapViewDelegate
+/*!
+ @brief 地图加载完成
+ @param mapview 地图View对象
+ */
+- (void)mapViewDidFinishLoadingMap:(FMKMapView *)mapView {
+    NSLog(@"地图加载完成");
+}
+/*!
+ @brief 地图加载失败
+ @param mapView 地图View
+ @param error 错误信息
+ */
+- (void)mapViewDidFailLoadingMap:(FMKMapView *)mapView withError:(NSError *)error {
+    NSLog(@"地图加载失败-%@", error);
 }
 
 
