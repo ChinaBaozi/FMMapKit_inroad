3ee839eb3165ab82c761a19a19cef286324ae7c1
ypcai <chinabaozi@yeah.net>
2018-01-28 15:06:08 +0800

    提交xcode工程

---
+++ FMMapKit_inroad/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FMMapKit_inroad
+//
+//  Created by roadPro on 2018/1/28.
+//  Copyright © 2018年 SQLlitedemo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ FMMapKit_inroad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FMMapKit_inroad
+//
+//  Created by roadPro on 2018/1/28.
+//  Copyright © 2018年 SQLlitedemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
